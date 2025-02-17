c5610dbcb48bc6b58b02aa5d03e6b638479d4dcc
Iñaki <innaki.delafuente@estudidante.uam.es>
2025-02-17 17:02:42 +0100

    -I

--- gitCommit.sh
+++ gitCommit.sh
@@ -6,6 +6,28 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# Verifica si se pasó el usuario a configurar
+while getopts "IJ" opt; do
+    case ${opt} in
+        I)
+            # Configura los datos de usuario para Iñaki
+            git config --global user.email "innaki.delafuente@estudidante.uam.es"
+            git config --global user.name "Iñaki"
+            echo "Configuración de usuario: Iñaki"
+            ;;
+        J)
+            # Configura los datos de usuario para Freddy
+            git config --global user.email "freddy.serrano.zurita@gmail.com"
+            git config --global user.name "Freddy Serrano Zurita"
+            echo "Configuración de usuario: Freddy Serrano Zurita"
+            ;;
+        *)
+            echo "Opción no válida. Usa -I para Iñaki o -J para Freddy."
+            exit 1
+            ;;
+    esac
+done
+
 # Agrega todos los cambios
 git add .
 
